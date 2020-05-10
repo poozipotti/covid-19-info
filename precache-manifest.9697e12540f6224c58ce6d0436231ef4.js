5aecd137b28a01f9c422e0e105ee4cb3fc6e359a
George Beckert <gbeckert@stagefronttickets.com>
2020-05-09 23:52:13 -0400

    Updates

---
+++ precache-manifest.9697e12540f6224c58ce6d0436231ef4.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfd023523709c48d3ca7889f4b9cf994",
+    "url": "/covid-19-info/index.html"
+  },
+  {
+    "revision": "45408b9728a8ec9c977c",
+    "url": "/covid-19-info/static/css/main.4804d39d.chunk.css"
+  },
+  {
+    "revision": "2ceb26f06ac6fbf56a01",
+    "url": "/covid-19-info/static/js/2.d7b31d24.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/covid-19-info/static/js/2.d7b31d24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45408b9728a8ec9c977c",
+    "url": "/covid-19-info/static/js/main.383e70ef.chunk.js"
+  },
+  {
+    "revision": "b55193b999e25dbbfccc",
+    "url": "/covid-19-info/static/js/runtime-main.d67af5bf.js"
+  },
+  {
+    "revision": "557d845bc1e89e73b1bddb6bf6aa1016",
+    "url": "/covid-19-info/static/media/Cough-Small.557d845b.svg"
+  },
+  {
+    "revision": "e1f25e6d9807ad7845e249bb3a5187db",
+    "url": "/covid-19-info/static/media/Woman-Mask.e1f25e6d.svg"
+  },
+  {
+    "revision": "4d0f034e15f046de378310cbd67eebbf",
+    "url": "/covid-19-info/static/media/close.4d0f034e.svg"
+  },
+  {
+    "revision": "1ebea521f6148ec0e1ae537581be4ad5",
+    "url": "/covid-19-info/static/media/far.1ebea521.svg"
+  },
+  {
+    "revision": "428182ba25fd951286abe1a7f39965e9",
+    "url": "/covid-19-info/static/media/hands.428182ba.svg"
+  },
+  {
+    "revision": "62a8690c4b67e2976e0b0525e166f139",
+    "url": "/covid-19-info/static/media/main.62a8690c.svg"
+  },
+  {
+    "revision": "c7471dea7280a6ea567499f061058ae1",
+    "url": "/covid-19-info/static/media/triangle.c7471dea.svg"
+  },
+  {
+    "revision": "769d1887a8bd2e04e54830bba98bbe8b",
+    "url": "/covid-19-info/static/media/virus.769d1887.svg"
+  }
+]);
